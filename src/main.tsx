c62138afeb9198a743fe906289fc0009da726025
Diego <diegofrontdev@gmail.com>
2024-01-11 19:48:49 -0300

    importadas las fuentes

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,8 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import "./main.css";
+import "@fontsource-variable/montserrat";
+import "@fontsource-variable/raleway";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
