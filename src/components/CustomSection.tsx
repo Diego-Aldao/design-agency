294d0b0e90de795f2343d0c79d52d52a2201a2d6
Diego <diegofrontdev@gmail.com>
2024-01-15 18:49:52 -0300

    ahora el componetne recibe la prop nombreSeccion. Terminadas las clases bem y refactorizado

--- src/components/CustomSection.tsx
+++ src/components/CustomSection.tsx
@@ -3,21 +3,26 @@ interface Props {
   mainTitle: string;
   noTitle?: boolean;
   children: JSX.Element;
+  nombreSeccion: string;
 }
 
-const CustomSection = ({ navTitle, mainTitle, children, noTitle }: Props) => {
+const CustomSection = ({
+  navTitle,
+  mainTitle,
+  children,
+  noTitle,
+  nombreSeccion,
+}: Props) => {
   return (
-    <section className="section py-10 w-full grid-cols-1 lg:py-20 grid md:grid-cols-[150px,_calc(100%_-_150px)] lg:grid-cols-[200px,_calc(100%_-_200px)]">
-      <aside className="section__aside hidden md:flex h-full justify-center relative">
-        <span className="section__aside__text capitalize text-xs pt-1 min-w-[80px] max-w-[80px] h-fit sticky top-10 lg:top-16 left-0 font-semibold">
+    <section className="custom-section" id={nombreSeccion}>
+      <aside className="custom-section__aside">
+        <span className="custom-section__aside__text text-aside">
           {navTitle}
         </span>
       </aside>
-      <div className="section__content flex flex-col gap-10 w-full">
-        <header className={`${noTitle && "header--disabled md:hidden"}`}>
-          <h2 className="section__content__title capitalize font-semibold text-3xl lg:text-4xl xl:text-5xl font-main-font">
-            {mainTitle}
-          </h2>
+      <div className="custom-section__content">
+        <header className={noTitle ? "md:hidden" : ""}>
+          <h2 className="custom-section__content__title ">{mainTitle}</h2>
         </header>
 
         {children}
