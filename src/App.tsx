33e813c9c5e3532544001df879cf37903f9004b8
Diego <diegofrontdev@gmail.com>
2024-01-15 18:50:11 -0300

    añadidos todos los componentes de la pagina

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,54 @@
+import CustomSection from "./components/CustomSection";
+import SectionPrincipal from "./components/SectionPrincipal";
+import MainLayout from "./layouts/MainLayout";
+
+import BreakSection from "./components/BreakSection";
+
+import FormularioContacto from "./components/FormularioContacto";
+import SectionTrabajos from "./components/SectionTrabajos";
+import SectionServicios from "./components/SectionServicios";
+import SectionExperiencia from "./components/SectionExperiencia";
+
 function App() {
-  return <></>;
+  return (
+    <>
+      <MainLayout>
+        <main className="main">
+          <SectionPrincipal />
+          <CustomSection
+            navTitle="trabajos"
+            mainTitle="nuestros trabajos"
+            nombreSeccion="trabajos"
+          >
+            <SectionTrabajos />
+          </CustomSection>
+          <CustomSection
+            navTitle="servicios"
+            mainTitle="nuestros servicios"
+            nombreSeccion="servicios"
+          >
+            <SectionServicios />
+          </CustomSection>
+          <BreakSection />
+          <CustomSection
+            navTitle="experiencia"
+            noTitle={true}
+            mainTitle="nuestra experiencia"
+            nombreSeccion="experiencia"
+          >
+            <SectionExperiencia />
+          </CustomSection>
+          <CustomSection
+            navTitle="cuéntanos sobre ti y el proyecto"
+            mainTitle="contáctanos"
+            nombreSeccion="contacto"
+          >
+            <FormularioContacto />
+          </CustomSection>
+        </main>
+      </MainLayout>
+    </>
+  );
 }
 
 export default App;
