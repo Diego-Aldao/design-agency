304c8346f91e15070c85a7b0b2c5220406ec4bc0
Diego <diegofrontdev@gmail.com>
2024-01-16 15:57:29 -0300

    añadida la seccion break

---
+++ src/components/BreakSection.tsx
@@ -0,0 +1,13 @@
+import imagenBreak from "../assets/ImagenSectionBreak.webp";
+
+const BreakSection = () => {
+  return (
+    <section className="section-break">
+      <div className="section-break__image">
+        <img src={imagenBreak} alt="" />
+      </div>
+    </section>
+  );
+};
+
+export default BreakSection;
